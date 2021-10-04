2a61c0276a5071192b25eb0d6ea9c03274a76168
Jonty <501211312@qq.com>
2021-10-04 17:00:09 +0800

    add generic method

--- new_src/sample.code/sample1.generic/Program.cs
+++ new_src/sample.code/sample1.generic/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using sample1.generic;
+
 Console.WriteLine("Hello, World!");
+
+
+var i = 123;
+var s = "Jonty";
+var d = DateTime.Now;
+
+Console.WriteLine("***************CommonMethod*************");
+// int
+CommonMethod.ShowTypeName(i);
+// string
+CommonMethod.ShowTypeName(s);
+// datetime
+CommonMethod.ShowTypeName(d);
+
+Console.WriteLine("***************ObjectMethod*************");
+// int
+CommonMethod.ShowObjectName(i);
+// string
+CommonMethod.ShowObjectName(s);
+// datetime
+CommonMethod.ShowObjectName(d);
+
+
+Console.WriteLine("***************GenericMethod*************");
+// int
+CommonMethod.ShowName(i);
+// string
+CommonMethod.ShowName(s);
+// datetime
+CommonMethod.ShowName(d);
+
+Console.ReadKey();
